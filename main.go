be168fa185383662824ff1b4b5e75b654b7d7f08
Victor Vecchio <victorvecchio.rva@gmail.com>
2024-01-28 17:50:27 -0300

    Continuando projeto

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "honnef.co/go/tools/printf"
+import "github.com/victor-rva/projeto01_GO/internal/entity"
+
+//import "honnef.co/go/tools/printf"
 
 type Car struct {
 	Model string
@@ -9,7 +11,17 @@ type Car struct {
 
 // metodo
 func (c Car) Start() {
-	println(c.Model + "is started")
+	println(c.Model + "has been started")
+}
+
+// func (c Car) ChangeColor(color string){
+// 	c.Color = color // duplicando o valor de c.color na memoria
+// 	println("New color: " + c.Color)
+// }
+
+func (c *Car) ChangeColor(color string){ // o * serve como ponteiro para indicar que a mudança do valor vai ser direto da memória we não uma cópia
+	c.Color = color
+	println("New color: " + c.Color)
 }
 
 // funcao
@@ -18,10 +30,24 @@ func soma(x int, y int) int {
 }
 
 func main() {
-	car := Car{ // declarando e atribuindo variavel car, serve para não precisar declarar antes a variavel
-		Model: "Ferrari",
-		Color: "Red",
+	order, err := entity.NewOrder("1",10,1)
+	if err != nil{
+		println(err.Error())
+	} else{
+		println(order.ID)
 	}
-	car.Model = "Fiat" // alterando o valor do atributo Model
-	println(car.Model)
 }
+
+// func main() {
+// 	car := Car{ // declarando e atribuindo variavel car, serve para não precisar declarar antes a variavel
+// 		Model: "Ferrari",
+// 		Color: "Red",
+// 	}
+// 	car.Start()
+// 	car.ChangeColor("Blue")
+// 	println(car.Color)
+// 	car.Model = "Fiat" // alterando o valor do atributo Model
+// 	println(car.Model)
+// }
+
+//para pegar o endereço de memória se utiliza & do lado da variavel
